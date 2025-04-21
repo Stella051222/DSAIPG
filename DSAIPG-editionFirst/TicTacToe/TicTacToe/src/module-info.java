f6719ce9163db7614c96c7458c4e97ef862f8a8c
Stella051222 <xstella06@gmail.com>
2025-04-20 23:42:43 -0400

    finished

--- DSAIPG-editionFirst/TicTacToe/TicTacToe/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module TicTacToe {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- DSAIPG-editionFirst/src/main/java/com/phasmidsoftware/dsaipg/projects/mcts/core/Position.java
+++
@@ -1,9 +0,0 @@
-package com.phasmidsoftware.dsaipg.projects.mcts.core;
-
-public interface Position<G extends Game> {
-    static <G extends Game> int[] Getmove(G position, G position1) {
-        return new int[0];
-    }
-
-
-}
